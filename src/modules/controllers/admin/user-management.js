05cc4585dd8df150036c255cfcb51532a6f0ee5b
kulrapat <kkulrapat@gmail.com>
2025-10-12 00:08:33 +0700

    แก้ไข user ไม่ครบ

--- src/modules/controllers/admin/user-management.js
+++ src/modules/controllers/admin/user-management.js
@@ -1,13 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
 const { ErrorCodes } = require("../../../exception/root");
-const NotFoundException  = require("../../../exception/not-found");
-const { UpdateUserSchema } = require('../../../schema/users');
-const bcrypt = require('bcrypt');
+const NotFoundException = require("../../../exception/not-found");
+const { UpdateUserSchema } = require("../../../schema/users");
+const bcrypt = require("bcrypt");
 const prisma = new PrismaClient();
 
 const GetUsers = async (req, res) => {
   const users = await prisma.users.findMany({
     select: {
+      id: true,
       employee_id: true,
       prefix: {
         select: {
@@ -37,17 +38,37 @@ const UpdateUser = async (req, res) => {
       first_name: body.first_name,
       last_name: body.last_name,
       username: body.username,
+      prefix_id: body.prefix_id,
       role_id: body.role_id,
     };
     if (body.password) {
       const salt = await bcrypt.genSalt(10);
       data.password = await bcrypt.hash(body.password, salt);
     }
+    await prisma.users.update({
+      where: { id },
+      data: data,
+    });
     await prisma.$executeRaw`UPDATE users SET updated_at = NOW() WHERE id = ${id}`;
-    res.json(data);
+    const updatedUser = await prisma.users.findUnique({
+      where: { id },
+      select: {
+        id: true, 
+        employee_id: true,
+        prefix: { select: { name: true } },
+        first_name: true,
+        last_name: true,
+        username: true,
+        role: { select: { role_name: true } },
+        last_login: true,
+        updated_at: true,
+      },
+    });
+
+    res.json(updatedUser);
   } catch (err) {
-    if (err?.code === 'P2025') {
-      throw new NotFoundException('User not found', ErrorCodes.USER_NOT_FOUND);
+    if (err?.code === "P2025") {
+      throw new NotFoundException("User not found", ErrorCodes.USER_NOT_FOUND);
     }
     throw err;
   }
@@ -57,12 +78,12 @@ const DeleteUser = async (req, res) => {
   try {
     const id = parseInt(req.params.id, 10);
     await prisma.users.delete({
-      where: { id }
+      where: { id },
     });
     return res.json({ message: "User deleted successfully" });
   } catch (err) {
-    if (err?.code === 'P2025') {
-      throw new NotFoundException('User not found', ErrorCodes.USER_NOT_FOUND);
+    if (err?.code === "P2025") {
+      throw new NotFoundException("User not found", ErrorCodes.USER_NOT_FOUND);
     }
     throw err;
   }
@@ -91,13 +112,13 @@ const GetUserById = async (req, res) => {
         },
         last_login: true,
         updated_at: true,
-      }
+      },
     });
-    
+
     if (!user) {
-      throw new NotFoundException('User not found', ErrorCodes.USER_NOT_FOUND);
+      throw new NotFoundException("User not found", ErrorCodes.USER_NOT_FOUND);
     }
-    
+
     res.json(user);
   } catch (err) {
     throw err;

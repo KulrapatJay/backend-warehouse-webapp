4d9d527bf61318373317be51a2d4952703c9033f
kulrapat <kkulrapat@gmail.com>
2025-10-22 01:29:07 +0700

    ทำ api ของ sales order โดยใช้ barcode แล้วเก็บ Order สำเร็จแล้ว

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const userManagementRouter = require('./user-management');
 const customersRouter = require('./customers');
 const productManagementRouter = require('./product-management');
 const productsWarehouseRouter = require('./product-warehouse');
+const salesOrdersRouter = require('./sales-orders');
 
 const rootRouter = express.Router();
 
@@ -12,5 +13,6 @@ rootRouter.use('/user-management', userManagementRouter);
 rootRouter.use('/customers', customersRouter);
 rootRouter.use('/products', productManagementRouter);
 rootRouter.use('/products-warehouse', productsWarehouseRouter);
+rootRouter.use('/sales-orders', salesOrdersRouter);
 
 module.exports = rootRouter;

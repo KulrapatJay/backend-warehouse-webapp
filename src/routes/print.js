4d448f9cf1820a9335aaf33067a7b24a56e26884
kulrapat <kkulrapat@gmail.com>
2025-11-02 01:18:02 +0700

    เพิ่มส่งข้อเครื่องพิมพ์ให้หน้า frontend

--- src/routes/print.js
+++ src/routes/print.js
@@ -1,17 +1,19 @@
-// const express = require('express');
-// const rateLimit = require('express-rate-limit');
-// const errorHandler = require('../error-handler');
-// const authMiddleware = require('../middlewares/auth');
-// const {staffOrManagerMiddleware} = require('../middlewares/role');
-// const { InitializePrinter, PrintSalesOrder, PrintCustomReceipt, TestPrint } = require('../modules/controllers/staff/print');
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const errorHandler = require('../error-handler');
+const authMiddleware = require('../middlewares/auth');
+const {staffOrManagerMiddleware} = require('../middlewares/role');
+const { GetSalesOrderForPrint, GetCustomReceiptData } = require('../modules/controllers/staff/print');
 
-// const apiLimit = rateLimit({
-//     windowMs: 1000 * 60 * 3, 
-//     max: 100,
-//     message: 'You have exceeded the 10 requests in 3 minutes limit!',
-// });
+const apiLimit = rateLimit({
+    windowMs: 1000 * 60 * 3, 
+    max: 100,
+    message: 'You have exceeded the 10 requests in 3 minutes limit!',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
-// const printRouter = express.Router();
+const printRouter = express.Router();
 
 // printRouter.post('/initialize', apiLimit, [authMiddleware, staffOrManagerMiddleware], errorHandler(InitializePrinter));
 // // Print sales order by ID
@@ -21,4 +23,11 @@
 // // Test print
 // printRouter.post('/test', apiLimit, [authMiddleware, staffOrManagerMiddleware], errorHandler(TestPrint));
 
-// module.exports = printRouter;
+printRouter.get('/sales-order/:orderId',apiLimit, [authMiddleware, staffOrManagerMiddleware], GetSalesOrderForPrint);
+
+
+printRouter.post('/custom-receipt',apiLimit, [authMiddleware, staffOrManagerMiddleware], GetCustomReceiptData);
+
+
+
+module.exports = printRouter;
